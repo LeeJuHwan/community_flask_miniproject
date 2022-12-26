d897bc1c65699377924e35b74e81b63ab5d174b8
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2022-12-26 17:42:54 +0900

    juhwan merge

--- flask_web/web_project/main/base/board_base.py
+++ flask_web/web_project/main/base/board_base.py
@@ -1,7 +1,20 @@
 from flask import Blueprint, render_template, redirect, url_for
+from ..db import conn, cur
+
 
 bp = Blueprint('board', __name__, url_prefix='/board')
 
 @bp.route("/")
 def select() :
     return render_template("board.html")
+
+@bp.route("test")
+def test() :
+    sql="""
+    SELECT username, b.title, b.content, b.write_date, t.lang, b.views,
+    (select count(*) from reply r where b.id = r.board_id) as cnt_reply from board b join user u on b.user_id = u.id join tag t on t.id = b.tag_id;
+    """
+    cur.execute(sql)
+    data_list = cur.fetchall()
+    print(data_list)
+    return render_template("test/test.html", data_list = data_list)
