2b7cb42fb127de23c97ef5723b284dc491936994
Jiye Roh <112691501+kkumtori@users.noreply.github.com>
2022-12-26 11:14:23 +0900

    [ADD]

--- flask_web/web_project/main/base/board_base.py
+++ flask_web/web_project/main/base/board_base.py
@@ -5,3 +5,7 @@ bp = Blueprint('board', __name__, url_prefix='/board')
 @bp.route("/")
 def select() :
     return render_template("board.html")
+
+@bp.route("/test")
+def test() :
+    return render_template("jiye.html")
